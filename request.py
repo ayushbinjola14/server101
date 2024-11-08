c9bb48f6f0c94d78fe4e6ff63c63008614b5fe08
Dino14 <ayushbinjola14@gmail.com>
2024-11-09 00:43:12 +0530

    socket & request library

---
+++ request.py
@@ -0,0 +1,5 @@
+import requests
+response = requests.get('http://aosabook.org/en/500L/web-server/testpage.html')
+print ('status code:'), response.status_code
+print ('content length:'), response.headers['content-length']
+print (response.text)
